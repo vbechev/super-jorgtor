121b7ef2385ec062e51d867a355a8cdab65f0c92
vbechev <victor.bechev@gmail.com>
2023-01-10 20:49:15 +0200

    Add collision

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ import sys
 pygame.init()
 
 # Constants
-SCREEN_DIMENSIONS = (1200, 800)
+SCREEN_DIMENSIONS = (1200, 896)
 BACKGROUND_COLOR = (150, 150, 255)
 SPRITE_SCALE = (100, 100) # Resize all sprites to 100x100
 FPS = 60
 
+
+def get_rect_dimensions(sprite_meta):
+    return (sprite_meta['x'],
+            sprite_meta['y'],
+            sprite_meta['width'],
+            sprite_meta['height'])
 
 class SpriteSheet:
     ENEMIES = 'students'
@@ -37,10 +43,7 @@ class SpriteSheet:
             pass
         sheet = self.sprite_sheets[sprite_type]
         sprite_meta = self.sprites_meta[sprite_type][sprite_id]
-        rect = pygame.Rect(sprite_meta['x'],
-                           sprite_meta['y'],
-                           sprite_meta['width'],
-                           sprite_meta['height'])
+        rect = pygame.Rect(get_rect_dimensions(sprite_meta))
         sprite = pygame.Surface((sprite_meta['width'], sprite_meta['height']))
         sprite.blit(sheet, (0, 0), rect)
         sprite = pygame.transform.scale(sprite, size)
@@ -60,45 +63,47 @@ class PlayerState(enum.Enum):
 
 class Player:
     MOVE_SPEED = 8
-    JUMP_FORCE = 8
-    MAX_VELOCITY = -25
+    JUMP_FORCE = 10
+    GRAVITY = 1
     MAX_GRAVITY = 16
+    MAX_JUMP = 30
     SPRITES = {PlayerState.WALKING: ['small_mario_{}'.format(i) for i in (1, 2, 3)],
                PlayerState.FALLING: ['small_mario_4'],
                PlayerState.JUMPING: ['small_mario_5'],
                PlayerState.STATIC: ['small_mario_7']}
     SPRITE_SHEET = SpriteSheet.TILE_SET
-    ANIMATION_DELAY = 10
+    ANIMATION_DELAY = 5
 
-    def __init__(self, x, y, screen, sprite_sheet):
+    def __init__(self, x, y, screen, sprite_sheet, world):
         # General-use instance setup
         self.screen = screen
         self.sprite_sheet = sprite_sheet
+        self.world = world
         # Setup the rectangle for the collision
         self.rect = pygame.Rect(0, 0, *SPRITE_SCALE)
         self.rect.x, self.rect.y = x, y
         # Setup the player state and sprite
+        self.can_jump = True
         self.velocity_x = 0
         self.velocity_y = 0
+        self.jump_hold = 0
+        # Stupid dependency towards generating the first frame of the player
         self.animation_count = 9001
         self.animation_frame = 0
         self.update_sprite()
 
     def update_sprite(self):
         if self.velocity_y < 0:
-            state = PlayerState.JUMPING
+            animation_state = PlayerState.JUMPING
         elif self.velocity_y > 0:
-            state = PlayerState.FALLING
+            animation_state = PlayerState.FALLING
         elif self.velocity_x != 0:
-            state = PlayerState.WALKING
+            animation_state = PlayerState.WALKING
         else:
-            state = PlayerState.STATIC
+            animation_state = PlayerState.STATIC
         flip = self.velocity_x < 0
-        sprites = self.SPRITES[state]
+        sprites = self.SPRITES[animation_state]
 
-        if state != PlayerState.WALKING:
-            # Always render static states
-            self.animation_count = 9001
         if self.animation_count >= self.ANIMATION_DELAY:
             # Safeguard when switching states
             if self.animation_frame >= len(sprites):
@@ -112,26 +117,38 @@ class Player:
         else:
             self.animation_count += 1
         self.screen.blit(self.sprite, self.rect)
-        # pygame.draw.rect(self.screen, (255, 255, 255), self.rect, 2)
-
-    def update(self):
+        pygame.draw.rect(self.screen, (255, 255, 255), self.rect, 5)
+    
+    def parse_controls(self):
+        # Reset sideways velocity in order not to check if a key is released
         self.velocity_x = 0
         key = pygame.key.get_pressed()
         if key[pygame.K_a]:
             self.velocity_x = -self.MOVE_SPEED
         if key[pygame.K_d]:
             self.velocity_x = self.MOVE_SPEED
-        if key[pygame.K_SPACE] and self.velocity_y > self.MAX_VELOCITY:
-            self.velocity_y -= self.JUMP_FORCE
-        
+        if key[pygame.K_SPACE] and self.can_jump:
+            self.velocity_y = -(self.JUMP_FORCE + self.jump_hold // self.JUMP_FORCE)
+            self.jump_hold -= 1
+            # Bug here with multiple jupms, oh well
+
+    def update_movement(self):
+        if self.velocity_y <= self.MAX_GRAVITY:
+            self.velocity_y += self.GRAVITY
+        if self.jump_hold == 0:
+            self.can_jump = False
+        for ground in self.world.ground:
+            if ground.colliderect(self.rect.x, self.rect.y + self.velocity_y, *SPRITE_SCALE) and self.velocity_y > 0:
+                # Fix velocity to the distance between the sprites
+                self.velocity_y = ground.top - self.rect.bottom
+                self.can_jump = True
+                self.jump_hold = self.MAX_JUMP
         self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
-        # Temp :)
-        if self.rect.bottom > SCREEN_DIMENSIONS[1]:
-            self.rect.bottom = SCREEN_DIMENSIONS[1]
-        if self.velocity_y < self.MAX_GRAVITY:
-            self.velocity_y += 1
 
+    def update(self):
+        self.parse_controls()
+        self.update_movement()
         self.update_sprite()
 
 
@@ -142,20 +159,24 @@ class Tile:
 class GameWorld:
     def __init__(self, screen):
         self.screen = screen
+        # Setup background
         self.background = pygame.image.load(f'graphics/level_1.png')
-        scale = SCREEN_DIMENSIONS[1] / self.background.get_height()
-        scaled_size = tuple(int(size * scale) for size in self.background.get_size())
-        self.background = pygame.transform.scale(self.background, scaled_size)
+        # Setup world
+        with open('jsons/level_1.json') as level_file:
+            world_meta = json.load(level_file)
+        self.ground = [pygame.Rect(get_rect_dimensions(ground)) for ground in world_meta['ground']]
     
     def render_world(self):
         self.screen.blit(self.background, (0, 0))
+        for ground_rect in self.ground:
+            pygame.draw.rect(self.screen, (0, 0, 255), ground_rect, 8)
 
 
 def main(screen):
     clock = pygame.time.Clock()
     sprite_sheet = SpriteSheet()
-    player = Player(600, 600, screen, sprite_sheet)
     world = GameWorld(screen)
+    player = Player(600, 600, screen, sprite_sheet, world)
     while True:
         clock.tick(FPS)
         world.render_world()
