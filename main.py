d26a48ec194aa339cf90aae0e29d54ba4e60a614
vbechev <victor.bechev@gmail.com>
2023-01-10 20:28:45 +0200

    Add animation to Jorgtor

--- main.py
+++ main.py
@@ -1,58 +1,142 @@
-import sys
+import enum
+import json
+import itertools
 import pygame
+import sys
 
 pygame.init()
 
 # Constants
-FPS = 60
-SCREEN_SIZE = (1200, 800)
+SCREEN_DIMENSIONS = (1200, 800)
 BACKGROUND_COLOR = (150, 150, 255)
+SPRITE_SCALE = (100, 100) # Resize all sprites to 100x100
+FPS = 60
 
-# Setup screen
+
+class SpriteSheet:
+    ENEMIES = 'students'
+    TILE_SET = 'tile_set'
+
+    def __init__(self):
+        with open(f'jsons/{self.ENEMIES}.json') as enemies_file:
+            enemies = json.load(enemies_file)
+        with open(f'jsons/{self.TILE_SET}.json') as tile_set_file:
+            tile_set = json.load(tile_set_file)
+        self.sprites_meta = {self.ENEMIES: enemies,
+                             self.TILE_SET: tile_set}
+        self.sprite_sheets = {
+            self.ENEMIES: pygame.image.load(f'graphics/{self.ENEMIES}.png').convert_alpha(),
+            self.TILE_SET: pygame.image.load(f'graphics/{self.TILE_SET}.png').convert_alpha(),
+        }
+
+    def get_sprite(self, sprite_type, sprite_id, position, size=SPRITE_SCALE, mirror=False):
+        sheet = self.sprite_sheets[sprite_type]
+        sprite_meta = self.sprites_meta[sprite_type][sprite_id]
+        rect = pygame.Rect(sprite_meta['x'],
+                           sprite_meta['y'],
+                           sprite_meta['width'],
+                           sprite_meta['height'])
+        sprite = pygame.Surface((sprite_meta['width'], sprite_meta['height']))
+        sprite.blit(sheet, (0, 0), rect)
+        sprite = pygame.transform.scale(sprite, size)
+        sprite.set_colorkey((0, 0, 0))
+        return sprite
+
+
+class PlayerState(enum.Enum):
+    STATIC = 1
+    WALKING = 2
+    AIRBORN = 3
 
 
 class Player:
-    MAX_VELOCITY = -10
-    MAX_GRAVITY = 20
+    MOVE_SPEED = 5
+    JUMP_FORCE = 5
+    MAX_VELOCITY = -15
+    MAX_GRAVITY = 10
+    SPRITES = {PlayerState.WALKING: ['small_mario_{}'.format(i) for i in (1, 2, 3)],
+               PlayerState.AIRBORN: ['small_mario_5'],
+               PlayerState.STATIC: ['small_mario_7']}
+    SPRITE_SHEET = SpriteSheet.TILE_SET
+    ANIMATION_DELAY = 10
 
-    def __init__(self, x, y, screen):
-        raw_image = pygame.image.load('graphics/jorgtor.png')
-        self.image = pygame.transform.scale(raw_image, (100, 100))
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = x, y
-        self.velocity_y = 0
+    def __init__(self, x, y, screen, sprite_sheet):
+        # General-use instance setup
         self.screen = screen
-    
+        self.sprite_sheet = sprite_sheet
+        # Setup the rectangle for the collision
+        self.rect = pygame.Rect(0, 0, *SPRITE_SCALE)
+        self.rect.x, self.rect.y = x, y
+        # Setup the player state and sprite
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.animation_count = 9001
+        self.animation_frame = 0
+        self.update_sprite()
+
+    def update_sprite(self):
+        if self.velocity_y != 10:
+            state = PlayerState.AIRBORN
+        elif self.velocity_x != 0:
+            state = PlayerState.WALKING
+        else:
+            state = PlayerState.STATIC
+        sprites = self.SPRITES[state]
+
+        if state != PlayerState.WALKING:
+            # Always render static states
+            self.animation_count = 9001
+        if self.animation_count >= self.ANIMATION_DELAY:
+            # Safeguard when switching states
+            if self.animation_frame >= len(sprites):
+                self.animation_frame = 0
+            sprite_name = sprites[self.animation_frame]
+            self.sprite = self.sprite_sheet.get_sprite(self.SPRITE_SHEET,
+                                                       sprite_name,
+                                                       (self.rect.x, self.rect.y))
+            self.animation_frame += 1
+            self.animation_count = 0
+        else:
+            self.animation_count += 1
+
     def update(self):
+        self.velocity_x = 0
         key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-            self.rect.x -= 5
-        if key[pygame.K_RIGHT]:
-            self.rect.x += 5
+        if key[pygame.K_a]:
+            self.velocity_x = -self.MOVE_SPEED
+        if key[pygame.K_d]:
+            self.velocity_x = self.MOVE_SPEED
         if key[pygame.K_SPACE] and self.velocity_y > self.MAX_VELOCITY:
-            self.velocity_y -= 10
-            
+            self.velocity_y -= self.JUMP_FORCE
+        
+        self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
+        # Temp :)
+        if self.rect.bottom > SCREEN_DIMENSIONS[1]:
+            self.rect.bottom = SCREEN_DIMENSIONS[1]
         if self.velocity_y < self.MAX_GRAVITY:
             self.velocity_y += 1
-            
-        self.screen.blit(self.image, self.rect)
+
+        self.update_sprite()
+        self.screen.blit(self.sprite, self.rect)
 
 
 def main(screen):
     clock = pygame.time.Clock()
-    player = Player(300, 600, screen)
+    sprite_sheet = SpriteSheet()
+    player = Player(600, 600, screen, sprite_sheet)
     while True:
         clock.tick(FPS)
         screen.fill(BACKGROUND_COLOR)
         player.update()
-        if any(event.type == pygame.QUIT for event in pygame.event.get()):
+        if any(event.type == pygame.QUIT
+            for event in pygame.event.get()):
             break
         pygame.display.update()
 
 
 if __name__ == '__main__':
-    screen = pygame.display.set_mode(SCREEN_SIZE)
+    screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
     pygame.display.set_caption('Super Jorgtor')
     main(screen)
     pygame.quit()
