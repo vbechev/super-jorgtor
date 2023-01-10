9559e8c759c6ae3c78364cd99d8f7cbdaa8fc1af
vbechev <victor.bechev@gmail.com>
2023-01-10 20:56:46 +0200

    Add enemies and obstacles

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_DIMENSIONS = (1200, 896)
 SCREEN_BUFFER = 0.5
 BACKGROUND_COLOR = (150, 150, 255)
 SPRITE_SCALE = (100, 100) # Resize all sprites to 100x100
-FPS = 60
+FPS = 100
 
 
 def get_rect_dimensions(sprite_meta):
@@ -139,18 +139,22 @@ class Player:
             self.velocity_y += self.GRAVITY
         if self.jump_hold == 0:
             self.can_jump = False
-        for world_object in self.world.objects:
-            if world_object.colliderect(self.rect.x, self.rect.y + self.velocity_y, *SPRITE_SCALE) and self.velocity_y > 0:
+        for world_object in self.world.collideable:
+            if world_object.rect.colliderect(self.rect.x, self.rect.y + self.velocity_y, *SPRITE_SCALE) and self.velocity_y > 0:
                 # Fix velocity to the distance between the sprites
-                self.velocity_y = world_object.top - self.rect.bottom
+                self.velocity_y = world_object.rect.top - self.rect.bottom
                 self.can_jump = True
                 self.jump_hold = self.MAX_JUMP
         self.rect.y += self.velocity_y
 
         # Handle horizontal movement
-        for world_object in self.world.objects:
-            if world_object.colliderect(self.rect.x + self.velocity_x, self.rect.y, *SPRITE_SCALE):
-                self.velocity_x = 0
+        for world_object in self.world.collideable:
+            if world_object.rect.colliderect(self.rect.x + self.velocity_x, self.rect.y, *SPRITE_SCALE):
+                # Fix velocity to the distance between the sprites
+                if self.velocity_x > 0:
+                    self.velocity_x = world_object.rect.left - self.rect.right
+                else:
+                    self.velocity_x = world_object.rect.right - self.rect.left
         if self.rect.right  >= SCREEN_DIMENSIONS[0] * SCREEN_BUFFER and self.velocity_x > 0:
             self.world.scroll(self.velocity_x)
             # Apart from scrolling the world, we need to keep the player in the screen
@@ -164,34 +168,60 @@ class Player:
         self.update_sprite()
 
 
-class Tile:
-    pass
+class WorldObject:
+    def __init__(self, object_meta):
+        self.rect = pygame.Rect(get_rect_dimensions(object_meta))
+
+
+class Enemy(WorldObject):
+    SPRITE_SHEET = SpriteSheet.ENEMIES
+
+    def __init__(self, object_meta, screen, sprite_sheet, world):
+        super().__init__(object_meta)
+        # General-use instance setup
+        self.screen = screen
+        self.sprite_sheet = sprite_sheet
+        self.world = world
+        self.sprite = self.sprite_sheet.get_sprite(self.SPRITE_SHEET,
+                                                   'student_1_enemy_1_step_1')
+        
+    def draw(self):
+        self.screen.blit(self.sprite, self.rect)
 
 
 class GameWorld:
-    def __init__(self, screen):
+    OBJECTS_TO_GENERATE = ['ground', 'pipe', 'stairs']
+    def __init__(self, screen, sprite_sheet):
         self.screen = screen
         # Setup background
         self.background = pygame.image.load(f'graphics/level_1.png')
         self.background_coordinates = [0, 0] # For scrolling
         # Setup world
+        self.offset = 0
         with open('jsons/level_1.json') as level_file:
             world_meta = json.load(level_file)
-        self.ground = [pygame.Rect(get_rect_dimensions(ground)) for ground in world_meta['ground']]
-        self.offset = 0
+        self.objects = [WorldObject(object_meta)
+                        for object_to_generate in self.OBJECTS_TO_GENERATE
+                        for object_meta in world_meta[object_to_generate]]
+        self.enemies = [Enemy(enemy_meta, screen, sprite_sheet, self)
+                        for enemy_meta in world_meta['enemy_1']]
         # Put a wall like in Trouman's Show
-        self.backwall = pygame.Rect(-10, 0, 10, 1000)
-        self.objects = self.ground + [self.backwall]
+        self.backwall = WorldObject({'x': -10,'y' :0, 'width': 10, 'height': 1000})
+        self.collideable = self.objects + self.enemies + [self.backwall]
     
     def render_world(self):
         self.screen.blit(self.background, self.background_coordinates)
-        for ground_rect in self.ground:
-            pygame.draw.rect(self.screen, (0, 0, 255), ground_rect, 8)
+        for enemy in self.enemies:
+            enemy.draw()
+        for world_object in self.collideable:
+            pygame.draw.rect(self.screen, (0, 0, 255), world_object.rect, 8)
     
     def scroll(self, offset):
         self.offset += offset
-        for ground in self.ground:
-            ground.x -= offset
+        for world_object in self.objects:
+            world_object.rect.x -= offset
+        for enemy in self.enemies:
+            enemy.rect.x -= offset
         self.background_coordinates[0] = -self.offset
         
 
@@ -199,8 +229,8 @@ class GameWorld:
 def main(screen):
     clock = pygame.time.Clock()
     sprite_sheet = SpriteSheet()
-    world = GameWorld(screen)
-    player = Player(600, 600, screen, sprite_sheet, world)
+    world = GameWorld(screen, sprite_sheet)
+    player = Player(200, 600, screen, sprite_sheet, world)
     while True:
         clock.tick(FPS)
         world.render_world()
